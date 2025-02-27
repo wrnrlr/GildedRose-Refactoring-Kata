92da6f54d20e6449cdab720d0d8bdb12afd324bd
wrnrlr <werner@alman.ax>
2025-02-27 15:07:25 +0100

    Refacor deno example

--- TypeScript-deno/app/gilded-rose.ts
+++ TypeScript-deno/app/gilded-rose.ts
@@ -18,60 +18,31 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality -
-              this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+    for (const item of this.items) {
+      if (item.name==='Sulfuras, Hand of Ragnaros') continue
+      item.sellIn--
+      item.quality = clamp(item.quality + getQualityDifference(item), 0, 50)
     }
-
-    return this.items;
+    return this.items
   }
 }
+
+// Return by how much the quality of an item should increase or decrease
+function getQualityDifference({name, sellIn, quality}: Item): number {
+  switch (name) {
+    // Ages Bries becomes better each day, doubely so after its expiration date
+    case 'Aged Brie': return sellIn < 0 ? 2 : 1
+    // Conjured Mana Cake becomes progressivaly worse closer to its expiration date
+    case 'Conjured Mana Cake': return sellIn > 4 ? -2 : -4
+    // A backstage pass becomes progressively more valuable closer to the date of the concert
+    // but loses all value after the concert
+    case 'Backstage passes to a TAFKAL80ETC concert':
+      return sellIn < 0 ? -quality :
+        sellIn < 5 ? 3 :
+        sellIn < 10 ? 2 : 1
+    // Every other product loses quality with each passing day, doubely so after its expiration date
+    default: return sellIn > 0 ? -1 : -2
+  }
+}
+
+const clamp = (val:number, min:number, max:number) => Math.min(Math.max(val, min), max)
